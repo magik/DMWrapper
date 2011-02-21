5532cdc2b93eab7d24bf27aeaf613fd74303dec7
magik <magik@slaps.me>
2011-02-21 13:26:21 -0800

    restructured intercept onCommand code

--- src/me/slaps/DMWrapper/DMWrapper.java
+++ src/me/slaps/DMWrapper/DMWrapper.java
@@ -114,98 +114,113 @@ public class DMWrapper extends JavaPlugin {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		// only respond to player commands
-		if ( !(sender instanceof Player) ) return false;
-		
-		if (cmd.getName().toLowerCase().equals("shop")) {
-			
-			if ( (args.length > 0) && (args[0].equalsIgnoreCase("location")) ) {
-				if (!hasPermission(sender, "location")) {
-					return false;
-				}
+		// location-based features only available to Players
+		if ( !(sender instanceof Player) ) return sm.onCommand(sender, cmd, commandLabel, args);
 
-				String pname = ((Player)sender).getName();						
-				if ( args.length == 2 && args[1].equalsIgnoreCase("set") ) {
-					sender.sendMessage("please right click the 1st corner");
-					cmdMap.put(pname, "location set");
-					return true;
-				} else if ( args.length == 2 && args[1].equalsIgnoreCase("cancel") ) {
-					if ( cmdMap.get(pname) == null ) {
-						sender.sendMessage("No operation to cancel");
-					} else {
-						sender.sendMessage("Operation canceled");
-						cmdMap.remove(pname);
-					}
-					return true;
-				} else if ( args.length == 2 && args[1].equalsIgnoreCase("check") ) {
-					Integer id = locMgr.getShopID(((Player)sender).getLocation());
-					if ( id > 0 ) {
-						sender.sendMessage("Shop ID: "+id);
-					} else {
-						sender.sendMessage("No shop location found here");
-					}
-					return true;
-				} else if ( args.length == 3 && args[1].equalsIgnoreCase("remove") ) {
-					if ( locMgr.removeShopByID(Integer.parseInt(args[2])) ) { 
-						sender.sendMessage("Shop Location removed");	
-					} else {
-						sender.sendMessage("Could not remove Shop ID: "+Integer.parseInt(args[2]));
-					}
-					return true;
-				} else if ( args.length == 2 && args[1].equalsIgnoreCase("enable") ) {
-					sender.sendMessage("Shop Locations enabled");
-					locMgr.enableShopLocations();
-					return true;
-				} else if ( args.length == 2 && args[1].equalsIgnoreCase("disable") ) {
-					sender.sendMessage("Shop Locations disabled");
-					locMgr.disableShopLocations();
-					return true;
-				} else if ( args.length == 2 && args[1].equalsIgnoreCase("list") ) {
-					sender.sendMessage("Shop IDs: "+locMgr.listShops());
-					return true;
-				} else if ( args.length == 3 && args[1].equalsIgnoreCase("tp") ) {
-					if ( sender instanceof Player ) {
-						Location dest = locMgr.getCenterOfShop(Integer.parseInt(args[2]));
-						if ( dest == null ) {
-							sender.sendMessage("Could not find shop.");
-						} else {
-							((Player)sender).teleportTo(dest);
-						}
-					}
-					return true;
-				} else {
-					// send player message on how to use /shop loc
-					sender.sendMessage("Usage: /shop location set - starts the shop setup process");
-					sender.sendMessage("         /shop location cancel - cancels setting a shop location");
-					sender.sendMessage("         /shop location check - checks ID of current location");
-					sender.sendMessage("         /shop location remove <ID> - removes the shop location");
-					sender.sendMessage("         /shop location enable - enables location based shops");
-					sender.sendMessage("         /shop location disable - disable location based shops");
-					sender.sendMessage("         /shop location list - lists shop IDs");
-					sender.sendMessage("         /shop location tp <ID> - teleports to shop");
-					return true; 
-				}
-			}
+		// only intercept shop command
+		if (cmd.getName().toLowerCase().equals("shop")) {	
 			
-			if ( args.length > 0 && locMgr.shopLocationsEnabled ) {
-				if ( args[0].equalsIgnoreCase("help") ) {
-					return sm.onCommand(sender,cmd,commandLabel,args);
-				} else if ( locMgr.inShopLoc(((Player)sender).getLocation()) ) {
-					return sm.onCommand(sender,cmd,commandLabel,args);
-				} else {
-					sender.sendMessage("Not in the shopping area!");
-					return true;
-				}
+			// pass commands to DynamicMarket or intercept
+			
+			// just '/shop'
+			if ( (args.length == 0) || ( args[0].equalsIgnoreCase("help") ) ){
+				Boolean ret = sm.onCommand(sender, cmd, commandLabel, args);
+				sender.sendMessage("        /shop location");
+				return true;
+				
+			// is location based shopping enabled?
+			} else if ( !locMgr.shopLocationsEnabled ) {
+				return sm.onCommand(sender, cmd, commandLabel, args);
+				
+			// locations enabled, intercept commands
 			} else {
-				return sm.onCommand(sender,cmd,commandLabel,args);
+				
+				// not a '/shop location' command
+				if ( !args[0].equalsIgnoreCase("location") ) {
+					
+					// in a shop location? or admin?
+					if ( hasPermission(sender, "admin") || locMgr.inShopLoc(((Player)sender).getLocation()) ) {
+						return sm.onCommand(sender, cmd, commandLabel, args);
+					} else {
+						sender.sendMessage("Not in the shopping area!");
+						return true;
+					}
+					
+				// a '/shop location' command
+				} else {
+					if (!hasPermission(sender, "location"))	return false;
+
+					String pname = ((Player)sender).getName();			
+					
+					if ( args.length == 2 && args[1].equalsIgnoreCase("set") ) {
+						sender.sendMessage("please right click the 1st corner");
+						cmdMap.put(pname, "location set");
+						return true;
+					} else if ( args.length == 2 && args[1].equalsIgnoreCase("cancel") ) {
+						if ( cmdMap.get(pname) == null ) {
+							sender.sendMessage("No operation to cancel");
+						} else {
+							sender.sendMessage("Operation canceled");
+							cmdMap.remove(pname);
+						}
+						return true;
+					} else if ( args.length == 2 && args[1].equalsIgnoreCase("check") ) {
+						Integer id = locMgr.getShopID(((Player)sender).getLocation());
+						if ( id > 0 ) {
+							sender.sendMessage("Shop ID: "+id);
+						} else {
+							sender.sendMessage("No shop location found here");
+						}
+						return true;
+					} else if ( args.length == 3 && args[1].equalsIgnoreCase("remove") ) {
+						if ( locMgr.removeShopByID(Integer.parseInt(args[2])) ) { 
+							sender.sendMessage("Shop Location removed");	
+						} else {
+							sender.sendMessage("Could not remove Shop ID: "+Integer.parseInt(args[2]));
+						}
+						return true;
+					} else if ( args.length == 2 && args[1].equalsIgnoreCase("enable") ) {
+						sender.sendMessage("Shop Locations enabled");
+						locMgr.enableShopLocations();
+						return true;
+					} else if ( args.length == 2 && args[1].equalsIgnoreCase("disable") ) {
+						sender.sendMessage("Shop Locations disabled");
+						locMgr.disableShopLocations();
+						return true;
+					} else if ( args.length == 2 && args[1].equalsIgnoreCase("list") ) {
+						sender.sendMessage("Shop IDs: "+locMgr.listShops());
+						return true;
+					} else if ( args.length == 3 && args[1].equalsIgnoreCase("tp") ) {
+						if ( sender instanceof Player ) {
+							Location dest = locMgr.getCenterOfShop(Integer.parseInt(args[2]));
+							if ( dest == null ) {
+								sender.sendMessage("Could not find shop.");
+							} else {
+								((Player)sender).teleportTo(dest);
+							}
+						}
+						return true;
+					} else {
+						// send player message on how to use /shop loc
+						sender.sendMessage("Usage: /shop location set - starts the shop setup process");
+						sender.sendMessage("         /shop location cancel - cancels setting a shop location");
+						sender.sendMessage("         /shop location check - checks ID of current location");
+						sender.sendMessage("         /shop location remove <ID> - removes the shop location");
+						sender.sendMessage("         /shop location enable - enables location based shops");
+						sender.sendMessage("         /shop location disable - disable location based shops");
+						sender.sendMessage("         /shop location list - lists shop IDs");
+						sender.sendMessage("         /shop location tp <ID> - teleports to shop");
+						return true; 
+					}					
+					
+				}
 			}
 			
-		} else { 
-			
+		} else {
 			return false;
-			
 		}
-		
+			
+			
 	}
 	
 	
