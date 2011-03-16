f349e58a183aeba56347a87459f6ceb1090b09dc
magik <magiik1@gmail.com>
2011-03-16 15:30:14 -0700

    added better player location tracking

--- src/me/slaps/DMWrapper/DMWrapperPlayerListener.java
+++ src/me/slaps/DMWrapper/DMWrapperPlayerListener.java
@@ -1,5 +1,7 @@
 package me.slaps.DMWrapper;
 
+import java.util.HashMap;
+
 import org.bukkit.Location;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
@@ -9,6 +11,8 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class DMWrapperPlayerListener extends PlayerListener {
 	
 	DMWrapper parent;
+	
+	protected HashMap<String, Boolean> inShopMap = new HashMap<String, Boolean>();
 
 	public DMWrapperPlayerListener(DMWrapper plug) {
 		parent = plug;
@@ -17,24 +21,29 @@ public class DMWrapperPlayerListener extends PlayerListener {
 	
 	@Override
 	public void onPlayerMove(PlayerMoveEvent event) {
-		if ( !parent.locMgr.shopLocationsEnabled ) return;
-		
-		Location from = event.getFrom();
+        if ( !parent.locMgr.shopLocationsEnabled ) return;
+	    
+	    String playerName = event.getPlayer().getName();
+	    
+	    if (!inShopMap.containsKey(playerName)) inShopMap.put(playerName,false);
+	    
 		Location to = event.getTo();
-		Location blockFrom = new Location(from.getWorld(), from.getBlockX(), from.getBlockY(), from.getBlockZ() );
-		Location blockTo = new Location(to.getWorld(), to.getBlockX(), to.getBlockY(), to.getBlockZ() );
+		Location blockTo = new Location(to.getWorld(), to.getX(), to.getY(), to.getZ() );
 		
-		if ( blockFrom.equals(blockTo) ) return;
-
-		if ( parent.locMgr.inShopLoc(blockFrom) ) {
-			if ( !parent.locMgr.inShopLoc(blockTo) ) {
-				event.getPlayer().sendMessage("Leaving the shopping area.");
-			}
+		Boolean inShop = inShopMap.get(playerName);
+		
+		if ( parent.locMgr.inShopLoc(blockTo) ) {
+		    if ( !inShop ) {
+		        event.getPlayer().sendMessage("Entered the shopping area.");
+		        inShopMap.put(playerName, true);
+		    }
 		} else {
-			if ( parent.locMgr.inShopLoc(blockTo) ) {
-				event.getPlayer().sendMessage("Entered the shopping area.");
-			}
+		    if ( inShop ) {
+                event.getPlayer().sendMessage("Leaving the shopping area.");
+                inShopMap.put(playerName, false);
+		    }
 		}
+		    
 		
 	}	
 }
